f7d841d557eb73037b68616998fbdbe7b81ce05a
Juan Hoyos <juan.hoyos4@gmail.com>
2023-11-12 22:54:39 -0500

    Lesson

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function showModal() {
-  const modal = document.getElementById("new-project-modal")
+function showModal(id) {
+  const modal = document.getElementById(id)
   modal.showModal()
 }
 
 // This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
 const newProjectBtn = document.getElementById("new-project-btn")
-newProjectBtn.addEventListener("click", showModal)
+if (newProjectBtn) {
+  newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")})
+} else {
+  console.warn("New projects button was not found")
+}
