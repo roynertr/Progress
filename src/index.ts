ae703559fa9bc96999c20a345d345f4825e6456c
Juan Hoyos <juan.hoyos4@gmail.com>
2023-11-17 14:32:53 -0500

    Lesson

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min"
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 import { IProject, ProjectStatus, UserRole } from "./classes/Project"
 import { ProjectsManager } from "./classes/ProjectsManager"
@@ -112,3 +113,21 @@ function renderScene() {
 }
 
 renderScene()
+
+const axes = new THREE.AxesHelper()
+const grid = new THREE.GridHelper()
+grid.material.transparent = true
+grid.material.opacity = 0.4
+grid.material.color = new THREE.Color("#808080")
+
+scene.add(axes, grid)
+
+const gui = new GUI()
+
+const cubeControls = gui.addFolder("Cube")
+
+cubeControls.add(cube.position, "x", -10, 10, 1)
+cubeControls.add(cube.position, "y", -10, 10, 1)
+cubeControls.add(cube.position, "z", -10, 10, 1)
+cubeControls.add(cube, "visible")
+cubeControls.addColor(cube.material, "color")
