13b97be83094fe726848308ffd61475636c993a0
Juan Hoyos <juan.hoyos4@gmail.com>
2023-11-17 14:24:04 -0500

    Lesson

--- src/classes/ProjectsManager.ts
+++ src/classes/ProjectsManager.ts
@@ -24,6 +24,13 @@ export class ProjectsManager {
       throw new Error(`A project with the name "${data.name}" already exists`)
     }
     const project = new Project(data)
+    project.ui.addEventListener("click", () => {
+      const projectsPage = document.getElementById("projects-page")
+      const detailsPage = document.getElementById("project-details")
+      if (!(projectsPage && detailsPage)) { return }
+      projectsPage.style.display = "none"
+      detailsPage.style.display = "flex"
+    })
     this.ui.append(project.ui)
     this.list.push(project)	
     return project
