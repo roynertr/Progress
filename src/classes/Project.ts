90efb0046b675964076017104f2d0ef9dca689c8
Juan Hoyos <juan.hoyos4@gmail.com>
2023-11-17 14:16:31 -0500

    Lesson

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -10,17 +10,57 @@ export interface IProject {
 }
 
 export class Project implements IProject {
-	name: string
+	//To satisfy IProject
+  name: string
 	description: string
 	status: "pending" | "active" | "finished"
 	userRole: "architect" | "engineer" | "developer"
-	finishDate: Date
+  finishDate: Date
+  
+  //Class internals
+  ui: HTMLDivElement
+  cost: number = 0
+  progress: number = 0
 
   constructor(data: IProject) {
+    //Project data definition
     this.name = data.name
     this.description = data.description
     this.status = data.status
     this.userRole = data.userRole
     this.finishDate = data.finishDate
+    this.setUI()
+  }
+
+  setUI() {
+    if (this.ui) {return}
+    this.ui = document.createElement("div")
+    this.ui.className = "project-card"
+    this.ui.innerHTML = `
+    <div class="card-header">
+      <p style="background-color: #ca8134; padding: 10px; border-radius: 8px; aspect-ratio: 1;">HC</p>
+      <div>
+        <h5>${this.name}</h5>
+        <p>${this.description}</p>
+      </div>
+    </div>
+    <div class="card-content">
+      <div class="card-property">
+        <p style="color: #969696;">Status</p>
+        <p>${this.status}</p>
+      </div>
+      <div class="card-property">
+        <p style="color: #969696;">Role</p>
+        <p>${this.userRole}</p>
+      </div>
+      <div class="card-property">
+        <p style="color: #969696;">Cost</p>
+        <p>$${this.cost}</p>
+      </div>
+      <div class="card-property">
+        <p style="color: #969696;">Estimated Progress</p>
+        <p>${this.progress * 100}%</p>
+      </div>
+    </div>`
   }
 }
